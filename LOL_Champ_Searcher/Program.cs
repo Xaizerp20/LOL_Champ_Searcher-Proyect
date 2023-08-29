e8abfcec580352e547631c0350dafa1a607af134
Xavier_Jimenez <xavierjp1220@gmail.com>
2023-08-29 11:12:46 -0400

    Create LoL Champ Seacher Proyect

---
+++ LOL_Champ_Searcher/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LOL_Champ_Searcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
